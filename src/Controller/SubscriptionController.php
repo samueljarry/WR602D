b51c8df0d9044247c8395769bb66dfd0165f892f
samueljarry <samueljarry.sj@gmail.com>
2024-06-02 15:35:31 +0200

    feat: subscription view

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\SubscriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SubscriptionController extends AbstractController
 {
+    function __construct(SubscriptionRepository $subscriptionRepository)
+    {
+        $this->subscriptionRepository = $subscriptionRepository;
+    }
+
     #[Route('/subscription', name: 'app_subscription')]
     public function index(): Response
     {
+        $subscriptions = $this->subscriptionRepository->findAll();
+
         return $this->render('subscription/index.html.twig', [
-            'controller_name' => 'SubscriptionController',
+            'subscriptions' => $subscriptions,
         ]);
     }
 }
